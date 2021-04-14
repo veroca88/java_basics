8d15db31ef3c962c9fa7c4c248cf66d7612b015a
veroca88 <veronicadonoso1506@gmail.com>
2021-04-13 20:30:20 -0500

    Examples of data types

---
+++ Lab-Datatypes/src/VariableDeclaration.java
@@ -0,0 +1,21 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        boolean on = false;
+        System.out.println(on);
+
+        short s = 8;
+        System.out.println(s);
+
+        int i = -15;
+        System.out.println(i);
+
+        float f = 3839.1416345f;
+        System.out.println(f);
+
+        long l = 125348761234234L;
+        System.out.println(l);
+
+        char c = 'a';
+        System.out.println(c);
+    }
+}
