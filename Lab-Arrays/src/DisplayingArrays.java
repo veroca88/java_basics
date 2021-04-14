ec053f4f09da41635179516fc6bf257e776651dc
veroca88 <veronicadonoso1506@gmail.com>
2021-04-14 10:52:42 -0500

    Lab about array

---
+++ Lab-Arrays/src/DisplayingArrays.java
@@ -0,0 +1,11 @@
+public class DisplayingArrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[3];
+        intArray[0] = -5432;
+        intArray[2] = 12;
+        System.out.println(intArray[2]);
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(arr.length);
+    }
+}
