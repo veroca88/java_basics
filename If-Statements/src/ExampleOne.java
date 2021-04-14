cb49d42b0b0ab39bac0649e06318abec63525e29
veroca88 <veronicadonoso1506@gmail.com>
2021-04-14 13:08:46 -0500

    Example of boolean with if statement

---
+++ If-Statements/src/ExampleOne.java
@@ -0,0 +1,10 @@
+public class ExampleOne {
+    public static void main(String[] args) {
+        boolean b = true;
+        if (b){
+            System.out.println("inside the if-statement");
+        }
+
+        System.out.println("Outside of the if-statement");
+    }
+}
