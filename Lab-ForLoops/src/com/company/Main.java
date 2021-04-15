5cc6c184dac7f6a43d8450f08c833492b6aa012e
veroca88 <veronicadonoso1506@gmail.com>
2021-04-15 11:58:34 -0500

    About foor loops

---
+++ Lab-ForLoops/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++){
+            System.out.println(i);
+        }
+
+        /*Create an array of ten byte values.
+        The values can be any arbitrary numbers that you choose.
+        Now use a for-loop to iterate over the elements
+        in the array and print them to the console. */
+
+        byte[] b = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 0; i < b.length; i++){
+            System.out.println(b[i]);
+        }
+
+
+
+
+    }
+}
