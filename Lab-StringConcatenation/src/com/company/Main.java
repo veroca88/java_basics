49c5bfabba62825872d4c5ee94b88946a215036c
veroca88 <veronicadonoso1506@gmail.com>
2021-04-15 06:42:54 -0500

    Concatenation alphanumeric if we want to make an operation remember put (a+b)

---
+++ Lab-StringConcatenation/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Concatenate numbers
+        int x = 2;
+        int y = 3;
+        System.out.println("The sum of x and y is " + x + y);
+        // output The sum of x and y is 23
+
+        System.out.println("The sum of x and y is " + (x + y));
+        // output The sum of x and y is 5
+
+    }
+}
