5573209f9476d6e50b9164dace05587ac2f65884
veroca88 <veronicadonoso1506@gmail.com>
2021-04-15 05:28:02 -0500

    Lab about how to use switch

--- Switch-Statements/src/ExampleOne.java
+++ Switch-Statements/src/ExampleOne.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ExampleOne {
+public class ExampleOne {
+    public static void main(String[] args) {
+        int value = 9;
+
+        switch(value) {
+            case 2:
+                System.out.println("Lower");
+                break;
+            case 5:
+                System.out.println("Equal");
+                break;
+            case 8:
+                System.out.println("Greater");
+                break;
+            default:
+                System.out.println("Default");
+                break;
+
+        }
+
+    }
 }
